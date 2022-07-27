eec4f1e137a730a55619d24167bca54b91988e91
Griffin Berlstein <gberlstein@octoml.ai>
2022-07-27 13:45:32 -0400

    cleanup

--- interface.py
+++ interface.py
@@ -135,49 +135,6 @@ class IntrinsicInterface:
 create_interface = IntrinsicInterface.create_interface
 
 
-@consumes('test resource', 5)
-@function
-def test_mma_intrin():
-    @T.prim_func
-    def desc(a: T.handle, b: T.handle, c: T.handle) -> None:
-        A = T.match_buffer(a, (16, 16), align=128, offset_factor=1)
-        B = T.match_buffer(b, (16, 16), align=128, offset_factor=1)
-        C = T.match_buffer(c, (16, 16), align=128, offset_factor=1)
-
-        with T.block('root'):
-            T.reads(C[0:16, 0:16], A[0:16, 0:16], B[0:16, 0:16])
-            T.writes(C[0:16, 0:16])
-            for i, j, k in T.grid(16, 16, 16):
-                with T.block('update'):
-                    vii, vjj, vkk = T.axis.remap('SSR', [i, j, k])
-                    C[vii, vjj] = C[vii, vjj] + A[vii, vkk] * B[vjj, vkk]
-
-    @T.prim_func
-    def impl(a: T.handle, b: T.handle, c: T.handle) -> None:
-        A = T.match_buffer(a, (16, 16), align=128, offset_factor=1)
-        B = T.match_buffer(b, (16, 16), align=128, offset_factor=1)
-        C = T.match_buffer(c, (16, 16), align=128, offset_factor=1)
-
-        with T.block('root'):
-            T.reads(C[0:16, 0:16], A[0:16, 0:16], B[0:16, 0:16])
-            T.writes(C[0:16, 0:16])
-            T.evaluate(
-                T.tvm_mma_sync(
-                    C.data,
-                    C.elem_offset // 256,
-                    A.data,
-                    A.elem_offset // 256,
-                    B.data,
-                    B.elem_offset // 256,
-                    C.data,
-                    C.elem_offset // 256,
-                    dtype='handle',
-                )
-            )
-
-    return desc, impl
-
-
 # @constraint(CV('A') < 5)
 # @constraint(CV('B') <= 5)
 # # @constraint(CC(0) < CV('A') < 7)
